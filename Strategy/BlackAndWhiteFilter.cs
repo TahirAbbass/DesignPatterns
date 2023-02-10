13919326bf688c0f56a76fbf7a5c2a2a6a28b60c
Tahir.Abbas <Tahir.Abbas@bentley.com>
2023-02-10 10:43:38 +0500

    Add filters changes

---
+++ Strategy/BlackAndWhiteFilter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy
+    {
+    class BlackAndWhiteFilter : IFilter
+        {
+        public void Apply (string fileName)
+            {
+            Console.Write("Apply Black & White filter");
+            }
+        }
+    }
---
+++ Strategy/HighContrastFilter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy
+    {
+    class HighContrastFilter
+        {
+        public void Apply (string fileName)
+            {
+            Console.Write("Apply High Contrast filter");
+            }
+        }
+    }
