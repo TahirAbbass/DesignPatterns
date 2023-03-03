a65c930c52b6fd5d4a7fcb70a669e7d139963f62
Tahir.Abbas <Tahir.Abbas@bentley.com>
2023-03-03 10:55:17 +0500

    Add WordProcesorSample Code

---
+++ WordProcessorSample/Exporter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordProcessorSample
+    {
+    class Exporter
+        {
+        public void Export (IDocument document, IExportStrategy strategy)
+            {
+            strategy.Export(document);
+            }
+        }
+    }
---
+++ WordProcessorSample/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordProcessorSample
+    {
+    class Program
+        {
+        static void Main (string[] args)
+            {
+            WordProcessor processor = new WordProcessor();
+
+            string str = "this is first line";
+            processor.AddLine(str);
+
+            byte[] byt = new byte[str.Length];
+            for (int i = 0; i < str.Length; i++)
+                {
+                byt[i] = Convert.ToByte(str[i]);
+                }
+            processor.AddImage(byt);
+
+            Exporter exporter = new Exporter();
+            HTMLExport hTMLExport = new HTMLExport();
+
+            exporter.Export(processor, hTMLExport);
+
+            TextExport textExport = new TextExport();
+
+            exporter.Export(processor, textExport);
+            }
+        }
+    }
