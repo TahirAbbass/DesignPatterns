aa24d7025d97a76f754ff11cb6337cd36d247f67
Tahir.Abbas <Tahir.Abbas@bentley.com>
2023-03-10 10:56:56 +0500

    Make Wordprocessor through builder pattern

---
+++ Add2/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Add2
+    {
+    public interface IObserver
+        {
+        // Receive update from subject
+        void Update (ISubject subject);
+        }
+    public interface ISubject
+        {
+        // Attach an observer to the subject.
+        void Attach (IObserver observer);
+
+        // Detach an observer from the subject.
+        void Detach (IObserver observer);
+        // Notify all observers about an event.
+        void Notify ();
+        }
+    // The Subject owns some important state and notifies observers when the
+    // state changes.
+    public class Store : ISubject
+        {
+        // For the sake of simplicity, the Subject's state, essential to all
+        // subscribers, is stored in this variable.
+        public List<int> Items = new List<int>();
+
+        // List of subscribers. In real life, the list of subscribers can be
+        // stored more comprehensively (categorized by event type, etc.).
+        private List<IObserver> _observers = new List<IObserver>();
+
+        // The subscription management methods.
+        public void Attach (IObserver observer)
+            {
+            Console.WriteLine("Subject: Attached an observer.");
+            this._observers.Add(observer);
+            }
+
+        public void Detach (IObserver observer)
+            {
+            this._observers.Remove(observer);
+            Console.WriteLine("Subject: Detached an observer.");
+            }
+        // Trigger an update in each subscriber.
+        public void Notify ()
+            {
+            Console.WriteLine("Subject: Notifying observers...");
+
+            foreach (var observer in _observers)
+                {
+                observer.Update(this);
+                }
+            }
+        // that triggers a notification method whenever something important is
+        public void AddItem (int id)
+            {
+            Console.WriteLine("\nSubject: Adding new Item.");
+            this.Items.Add(id);
+            this.Notify();
+            }
+        }
+
+    // Concrete Observers react to the updates issued by the Subject they had
+    // been attached to.
+    class Customer : IObserver
+        {
+        public void Update (ISubject subject)
+            {
+             Console.WriteLine("Customer: Reacted to the event.");
+            }
+        }
+    class Program
+        {
+        static void Main (string[] args)
+            {
+            // The client code.
+            var store = new Store();
+            //store.AddItem(1);
+
+            var observer = new Customer();
+            store.Attach(observer);
+
+            //store.AddItem(2);
+
+            var observer2 = new Customer();
+            store.Attach(observer2);
+
+            store.AddItem(2);
+
+            }
+        }
+    }
---
+++ WordProcessorBuilderPattern/WordProcessor.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordProcessorSample
+    {
+    //enum  ExportFormat
+    //    {
+    //    TXT,
+    //    HTML
+    //    }
+
+    interface IDocument
+        {
+        void AddLine (string line);
+        void AddImage (byte[] img);
+        List<string> GetLines ();
+        List<byte[]> GetImages ();
+        }
+
+
+    class WordProcessor : IDocument
+        {
+        private List<string> Lines = new List<string>();
+        private List<byte[]> Images = new List<byte[]>();
+
+        public void AddLine (string line)
+            {
+            Lines.Add(line);
+            }
+
+        public void AddImage (byte[] img)
+            {
+            Images.Add(img);
+            }
+
+        public List<string> GetLines ()
+            {
+            return Lines;
+            }
+
+        public List<byte[]> GetImages ()
+            {
+            return Images;
+            }
+
+        }
+    }
